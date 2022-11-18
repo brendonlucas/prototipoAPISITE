1a0b11812f21e94599dc40215b240d9ea029344e
brendonlucas <brendonplay007@gmail.com>
2022-11-18 19:06:21 -0300

    first commit

--- prototipo/urls.py
+++ prototipo/urls.py
@@ -13,5 +13,8 @@ urlpatterns = [
     path('', include('veiculo.urls')),
     path('', include('ordem.urls')),
     path('', include('instituicao.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
