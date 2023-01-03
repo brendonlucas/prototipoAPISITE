c821895b346b3e3b6e5dd3c7e95444692a4666ee
brendonlucas <brendonplay007@gmail.com>
2023-01-03 18:05:59 -0300

    modificoes em todo o projeto

--- veiculo/urls.py
+++ veiculo/urls.py
@@ -6,5 +6,6 @@ from veiculo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('<pk>/veiculos/', views.show_veiculos, name='veiculos_show'),
+    path('<pk>/veiculos/<pk_2>/details', views.details_veiculo, name='details_veiculo'),
     path('<pk>/add_veiculo/', views.add_veiculo, name='create_veiculo'),
 ]
--- veiculo/views.py
+++ veiculo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 
 from instituicao.models import Instituicao
+from ordem.models import Ordem
+from usuario.models import Usuario
 from veiculo.forms import CreateVeiculo
 from veiculo.models import Veiculo, TipoVeiculo
 
@@ -9,24 +11,22 @@ def get_user_logged(request):
     return request.user
 
 
+def get_user(request):
+    return Usuario.objects.get(user=get_user_logged(request))
+
+
 def add_veiculo(request, pk):
     if request.method == 'POST':
         form = CreateVeiculo(request.POST)
-
         if form.is_valid():
             instituicao = Instituicao.objects.filter(id=pk).first()
-            print("*xczds-*-*-*-*-*-*-*-*-*-*--*--*-*-*-*-*-*-*-*", pk)
-
             if instituicao:
                 dados_form = form.data
                 tipo = TipoVeiculo.objects.get(id=form.data['tipo'])
-
-                print(form.data['name'], form.data['QtdCarga'], form.data['placa'], tipo.name, instituicao.nome)
-
-                veiculo = Veiculo(name=form.data['name'], qtd_pessoas=form.data['QtdCarga'], placa=form.data['placa'],
-                                  tipo=tipo, instituicao=instituicao).save()
+                veiculo = Veiculo(name=form.data['name'], qtd_pessoas=form.data['QtdCarga'],
+                                  placa=form.data['placa'].upper(), tipo=tipo, instituicao=instituicao).save()
                 return redirect('veiculos_show', pk)
-    return redirect('veiculos_show', pk)
+        return redirect('veiculos_show', pk)
 
 
 def edit_veiculo(request):
@@ -41,8 +41,14 @@ def show_veiculos(request, pk):
     instituicao = Instituicao.objects.get(id=pk)
     veiculos = Veiculo.objects.filter(instituicao=pk)
     return render(request, 'show_veiculos.html',
-                  {'user': get_user_logged(request), 'veiculos': veiculos, 'instituicao': instituicao})
+                  {'user': get_user_logged(request), 'veiculos': veiculos, 'instituicao': instituicao,
+                   'usuario': get_user(request)})
 
 
-def show_veiculo(request):
-    pass
+def details_veiculo(request, pk, pk_2):
+    veiculo = Veiculo.objects.get(id=pk_2, instituicao=pk)
+    instituicao = Instituicao.objects.get(id=pk)
+    ordens = Ordem.objects.filter(veiculo=pk_2, instituicao=pk, status_id__in=[2, 4, 1]).order_by('data_solicitacao')[:5]
+    return render(request, 'Details_veiculo.html',
+                  {'user': get_user_logged(request), 'veiculo': veiculo, 'instituicao': instituicao,
+                   'usuario': get_user(request), 'ordens': ordens})
